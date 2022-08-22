bdd554b7bcd6e282f1a2505ffde401f1b6053e57
personal <personal@DESKTOP-0J3T8MU>
2022-08-21 19:20:14 -0500

    Agregar archivos de proyecto.

---
+++ Productos/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos
+{
+    public class Class1
+    {
+        public int N { get; set; }
+        public int Precio { get; set; }
+        public int Cantidad { get; set; }
+        public int Moneda { get; set; }
+        public int Cambio { get; set; }
+        public string Nombre { get; set; }
+        public int Faltante { get; set; }
+    }
+}
